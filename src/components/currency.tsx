d49aafcb22847e6620eb39e63fb6bbaa00ec2568
Julio <beito_xp@me.com>
2024-05-30 02:37:23 -0600

    Currency Card updated

--- src/components/currency.tsx
+++ src/components/currency.tsx
@@ -2,11 +2,19 @@
 import {
     Card,
     CardContent,
-    CardDescription,
     CardHeader,
     CardTitle,
     CardFooter,
-} from "@/components/ui/card";
+} from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/select";
 
 interface CurrencyProps {
 
@@ -14,16 +22,32 @@ interface CurrencyProps {
 
 const Currency: React.FC<CurrencyProps> = ({ }) => {
     return (
-        <Card>
+        <Card className="w-[350px]">
             <CardHeader>
-                <CardTitle></CardTitle>
-                <CardDescription></CardDescription>
+                <CardTitle>Currency Converter</CardTitle>
             </CardHeader>
             <CardContent>
-                <p></p>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Amount</Label>
+                            <Input id="name" placeholder="placeholder" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="framework">From</Label>
+                            <Select>
+                                <SelectTrigger id="framework">
+                                    <SelectValue placeholder="Select" />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
             </CardContent>
-            <CardFooter>
-                <p></p>
+            <CardFooter className="flex justify-between">
             </CardFooter>
         </Card>
     );
